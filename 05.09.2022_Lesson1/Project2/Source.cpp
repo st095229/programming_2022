6ca2b02af2b08048d2a0c4cd3da04e0805ef5f66
st095229 <suxofructik@gmail.com>
2022-09-05 22:46:28 +0300

    Lesson 1 code upload

---
+++ 05.09.2022_Lesson1/Project2/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int a = 0;
+	int b = 0;
+	
+	cin >> a >> b;
+	cout << a + b * b << endl;
+
+	return EXIT_SUCCESS;
+}
