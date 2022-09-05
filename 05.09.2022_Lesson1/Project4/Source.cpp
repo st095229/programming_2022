313acfbef470ce32ccc5794b20e13a5126161a2a
st095229 <suxofructik@gmail.com>
2022-09-05 23:25:37 +0300

    lesson1

---
+++ 05.09.2022_Lesson1/Project4/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int a = 0;
+	int b = 0;
+
+	cin >> a >> b;
+
+	cout << b / a << endl;
+
+	return EXIT_SUCCESS;
+}
---
+++ 05.09.2022_Lesson1/Project5/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int a = 0;
+
+	cin >> a;
+
+	cout << a / 10 << endl;
+
+	return EXIT_SUCCESS;
+}
